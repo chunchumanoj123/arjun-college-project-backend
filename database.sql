d11c668d805c2bcbc4f188f967c770e5cbe14402
Manoj Chunchu <chunchumanoj2002@gmail.com>
2024-05-02 19:34:14 +0530

    my-first-commit

--- database.sql
+++ database.sql
@@ -71,33 +71,24 @@ create table if not exists block (
 
 create table if not exists student (
     student_id int primary key not null,
-    block_id int,
     usn text ,
-    room text,
-    foreign key (student_id) references users(user_id) on delete cascade,
-    foreign key (block_id) references block(block_id) on delete cascade
+    room text
 );
 
 create table if not exists warden (
-    warden_id int primary key not null,
-    block_id int,
-    foreign key (warden_id) references users(user_id) on delete cascade,
-    foreign key (block_id) references block(block_id)
-    on delete cascade
+    warden_id int primary key not null
 );
 
 create table if not exists complaint (
     id SERIAL PRIMARY KEY,
     name text ,
-    block_id int,
     student_id int ,
     description text,
     room text,
     is_completed BOOLEAN,
     created_at timestamp,
     assigned_at timestamp,
-    foreign key (student_id) references student(student_id) on delete cascade,
-    foreign key (block_id) references block(block_id) on delete cascade
+   
 );
 
 -- create table if not exists complaint (
